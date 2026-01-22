f74051e89b4d1d63654873ebc61e28c9ea82ef64
SkipperDon <skipperdon@atmyboat.com>
2026-01-21 20:35:01 -0500

    Update firstboot.sh

--- scripts/firstboot.sh
+++ scripts/firstboot.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
-
+export DEBIAN_FRONTEND=noninteractive
 echo "=== FIRST BOOT: Installing Helm System ==="
-
+sudo apt-get -o Dpkg::Options::="--force-confold" \
+             -o Dpkg::Options::="--force-confdef" \
+             install -y <packages>
 # Expand filesystem
 raspi-config nonint do_expand_rootfs
 
